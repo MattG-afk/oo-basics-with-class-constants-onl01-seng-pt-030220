a46d1959ba2083df081436432b3c0236548085d2
Matt Gordon <bigmattsize21@gmail.com>
2020-03-20 02:46:42 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,7 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   
-  Brands=[]
+  BRANDS=[]
 
   def initialize(brand)
     @brand = brand
@@ -16,7 +16,7 @@ class Shoe
   
   def brand
     @brand=brand
-    Brands<< brand
+    BRANDS<< brand
   end
 
 end
