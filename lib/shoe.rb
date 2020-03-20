b5858b4268dee3188faeb846976f574fd1284b92
Matt Gordon <bigmattsize21@gmail.com>
2020-03-20 02:45:02 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,6 +2,7 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   
+  Brands[]
 
   def initialize(brand)
     @brand = brand
@@ -11,5 +12,9 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+  
+  
+  def brand
+    
 
 end
